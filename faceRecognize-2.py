01278e25ededd3023b6662226a18eb9f52d52cd1
Vanyi Chao <71866403+chaovanyi@users.noreply.github.com>
2021-02-10 14:22:05 +0700

    Update faceRecognize-2.py

--- faceRecognize-2.py
+++ faceRecognize-2.py
@@ -1,1 +1,12 @@
+import cv2
+import face_recognition
+print(cv2.__version__)
 
+donFace = face_recognition.load_image_file('image path')
+
+
+
+
+if cv2.waitKey(1) == ord('q'):
+  break
+cv2.destroyAllWindow()
